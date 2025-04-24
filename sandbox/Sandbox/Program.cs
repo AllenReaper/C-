b3ec65fe1c2efe643cb4ddaa88db0f60e5475332
Jaxson McCammon <jaxman00@gmail.com>
2025-04-23 23:39:23 -0600

    ud 4/23

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Transactions;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World! you freaking legend!!");
+        // these are notes 
+        Console.WriteLine("Enter the radesis");
+        string text = Console.ReadLine();
+        double raduis = double.Parse(text);
+
+        //compute the area
+        double area = Math.PI * raduis * raduis;
+
+        Console.WriteLine($"here: {area}");
+        // "$" tells the consol to look in the line for variables.
+        
+          
+        string s = "goodbuy";
+        string x = "freak";
+        float f = 3.14f;
+        double d = 5.21;
+        
+        // <-- this is how you write notes in the code.
+        /* same
+        */ 
+        Console.WriteLine($"Hello {x} Sandbox World! {x} you {f} freaking {d} legend!! {s}");
+
+
+        Console.Write("whats your firstname?");
+        string firstname = Console.ReadLine();
+
+        Console.Write("whats your lastname?");
+        string lastname = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
