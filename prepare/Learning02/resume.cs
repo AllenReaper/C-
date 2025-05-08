59240f09d8d7ff234d5bec6880a3c03b161f7c3e
Jaxson McCammon <jaxman00@gmail.com>
2025-05-08 17:59:52 -0600

    unfinished UD 5/8/25

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 public class Resume
 {
     public string _name;
---
+++ prove/Develop02/Jounal.cs
@@ -0,0 +1,16 @@
+public class Jounal
+{
+    public void _AddingEntry()
+    {
+        
+    }
+
+    public string _DisplayEntries;
+    public string _SaveFile;
+    public string _loadtexttoFile;
+
+
+
+    
+
+}
